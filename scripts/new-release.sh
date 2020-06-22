17d77faced80499aeb309233ccf0095e19baf961
Damian Mee <spam@meedamian.com>
2020-06-22 22:30:08 +0800

    VERSION checks in new-release script

--- scripts/new-release.sh
+++ scripts/new-release.sh
@@ -6,15 +6,15 @@ set -e
 ## Given version, this script creates & pushes a relevant git-tag.
 #
 
-# required version
-VERSION=$1
-
-# Make sure $VERSION is provided before proceeding
-if [ -z "$VERSION" ]; then
+# Make sure $1 / $VERSION is provided before proceeding
+if [ -z "$1" ]; then
   >&2 printf "\nERR: version missing:  version needs to be passed as the first argument.  Try:\n"
   >&2 printf "\t./%s  %s\n\n"   "$(basename "$0")"  "v4.2.0"
   exit 1
 fi
+
+# Required version (make sure it always starts with v)
+VERSION="v${1#v}"
 
 # Verify there's no uncommitted changes
 if ! git diff-files --quiet; then
@@ -23,7 +23,7 @@ if ! git diff-files --quiet; then
 fi
 
 # Make sure specified $VERSION is present in Dockerfile
-if ! grep -q "$VERSION" ./Dockerfile; then
+if ! grep -q "VERSION=$VERSION" ./Dockerfile; then
   >&2 printf "\nERR: Requested version not present in Dockerfile. Make sure that's what you want to do.\n\n"
   exit 1
 fi
